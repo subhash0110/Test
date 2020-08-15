be5d7ada4f641fabb5730a24c1192c2b081b34b9
Rohith Varma <richardrk1243@gmail.com>
2020-08-15 23:30:26 +0530

    module setup

--- angular10/src/app/app-routing.module.ts
+++ angular10/src/app/app-routing.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/admin',
+    pathMatch: 'full',
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import(`../app/userinfo/userinfo.module`).then((m) => m.UserinfoModule),
+  },
 
-  const routes: Routes = [
-    {
-      path:'',
-      redirectTo:'/admin',
-      pathMatch:'full'
-    },
-    { path: 'admin', loadChildren: () => import(`../app/userinfo/userinfo.module`).then(m => m.UserinfoModule) },
-    ];
-
+  {
+    path: '',
+    redirectTo: '/cart',
+    pathMatch: 'full',
+  },
+  {
+    path: 'cart',
+    loadChildren: () =>
+      import(`../app/shopping-cart/shopping-cart.module`).then(
+        (m) => m.ShoppingCartModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
-// UserinfoRoutingModule
+export class AppRoutingModule {}
